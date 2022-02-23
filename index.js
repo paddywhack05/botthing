28c44b6e65c2a2be6dc810d5f20a080ecc2cda9f
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-02-23 23:47:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('IT is ready the bot')
+
+    client.user.setActivity("bot",{type: "being a"})
 })
 
 client.on('messageCreate', (message) => {
@@ -18,6 +20,17 @@ client.on('messageCreate', (message) => {
     if(message.author.bot){
         return;
     }
+
+    if (!message.channel === "🤖bot-commands🤖") {
+        message.reply({content: "go to bot commands if you want to use me"})
+        return;
+    }
+
+   // if (message.content === 'hi bot'|| message.content === 'Hi bot') {
+      //  message.reply({
+          //  content: "your codes have no power here "+ message.author.username,
+        
+      //  })
     if (message.content === 'hi bot'|| message.content === 'Hi bot') {
         message.reply({
             content: 'hello '+ message.author.username,
