5b15fa077d495b5ee6328a161b60088242865d44
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-02-23 21:17:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', (message) => {
-    if (message.content === 'hi bot') {
+    if (message.content === 'hi bot'|| message.content === 'Hi bot') {
         message.reply({
             content: 'hello',
             
