8647f626dfb77ba5b3cb9cf595b3f1d36b6cd6e9
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-02-24 17:13:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-import DiscordJS, { BaseGuild, Guild, GuildBan, Intents } from 'discord.js'
+import DiscordJS, { BaseGuild, Guild, Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
-// what my discord bot needs from discord
+
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_PRESENCES,
-
+        Intents.FLAGS.GUILD_MESSAGES
     ]
 })
-//ready message
+
 client.on('ready', () => {
     console.log('IT is ready the bot')
-   
-    //what my bots doing
 
+    client.user.setActivity("bot",{type: "being a"})
 })
-//when message create event
+
 client.on('messageCreate', (message) => {
 
     if(message.author.bot){
         return;
     }
+   // if (Guild.name ===)
 
-                //konami code command
     if (message.content === 'Up, Up, Down, Down, Left, Right, Left, Right, B, A'|| message.content === 'up, up, down, down, left, right, left, right, b, a'|| message.content === "Up Up Down Down Left Right Left Right B A"|| message.content === "up up down down left right left right b a") {
         message.reply({
+            content: 'hello '+ message.author.username,
             content: 'your codes have no power hear '+ message.author.username,
             
         })
     }
-                    //hi command
     if (message.content === 'hi bot'|| message.content === 'Hi bot') {
         message.reply({
             content: 'hello '+ message.author.username,
@@ -44,5 +40,5 @@ client.on('messageCreate', (message) => {
     
 })
 
-// my discord bot login with token in the env file
+
 client.login(process.env.TOKEN)
