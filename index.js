b92258cb16a86c84bd5f5c54966893c8afc5e49a
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-02-24 11:17:50 +0000

    more intents discord bot status people using bot

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-import DiscordJS, { BaseGuild, Guild, Intents } from 'discord.js'
+import DiscordJS, { BaseGuild, Guild, GuildBan, Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
-
+// what my discord bot needs from discord
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
 })
-
+//ready message
 client.on('ready', () => {
     console.log('IT is ready the bot')
 
+    //what my bots doing
+    client.on("ready",async()=> {
+        let servers = await client.guilds.cache.size
+        let servercount = await client.guilds.cache.reduce((a,b)=> a+b.memberCount,0 )
+
+        const activities =[
+            `?help | ${servers} servers`,
+        ]
+
+        setInterval(() =>{
+            const status = activities[Math.floor(Math.random()*activities.length)]
+            client.user.setPresence({activities:[{name: `${status}`}]})
+
+        }, 5000)
+    })
     client.user.setActivity("bot",{type: "being a"})
 })
-
+//when message create event
 client.on('messageCreate', (message) => {
 
     if(message.author.bot){
         return;
     }
-   // if (Guild.name ===)
 
+                //konami code command
     if (message.content === 'Up, Up, Down, Down, Left, Right, Left, Right, B, A'|| message.content === 'up, up, down, down, left, right, left, right, b, a'|| message.content === "Up Up Down Down Left Right Left Right B A"|| message.content === "up up down down left right left right b a") {
         message.reply({
             content: 'your codes have no power hear '+ message.author.username,
             
         })
     }
+                    //hi command
     if (message.content === 'hi bot'|| message.content === 'Hi bot') {
         message.reply({
             content: 'hello '+ message.author.username,
@@ -39,5 +55,5 @@ client.on('messageCreate', (message) => {
     
 })
 
-
+// my discord bot login with token in the env file
 client.login(process.env.TOKEN)
