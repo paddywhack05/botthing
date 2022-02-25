37c8b257c57bfee0db7cc6e52a89c385914d7ef4
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-02-25 12:17:19 +0000

    uh

--- commands/jokes.js
+++
@@ -1,8 +0,0 @@
-
-module.exports= {
-    name: 'jokes/randomgifs',
-    description:'when evr types bot !tell me a joke or random gif',
-     execute(message, args){
-        message.channel.send('yes it worked')
-        }
-}
--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-//import DiscordJS, { BaseGuild, Guild, Intents } from 'discord.js'
-const Discord = require('discord.js');
+import DiscordJS, { BaseGuild, Guild, Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
-//require('dotenv').config();
-//dotenv.config()
-const { Client, Intents} = require('discord.js');
-const client = new Discord.Client({ intents: 32767 });
-const fs = require('fs')
-
-client.commands = new Discord.Collection
-
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
- 
-    client.commands.set(command.name, command);
-}
-
-
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
 
 client.on('ready', () => {
     console.log('IT is ready the bot')
 
-});
-
+    client.user.setActivity("bot",{type: "being a"})
+})
 
 client.on('messageCreate', (message) => {
-    const command = args.shift().toLowerCase();
+
     if(message.author.bot){
         return;
     }
- 
-    if(message.content==='test bot 123'){
-        client.commands.get('jokes').execute(message,args);
-    }
+   // if (Guild.name ===)
 
     if (message.content === 'Up, Up, Down, Down, Left, Right, Left, Right, B, A'|| message.content === 'up, up, down, down, left, right, left, right, b, a'|| message.content === "Up Up Down Down Left Right Left Right B A"|| message.content === "up up down down left right left right b a") {
         message.reply({
+            content: 'hello '+ message.author.username,
             content: 'your codes have no power hear '+ message.author.username,
             
         })
@@ -52,4 +41,4 @@ client.on('messageCreate', (message) => {
 })
 
 
-client.login('OTE2NzQzODY2OTE1Mzg5NTQy.YaumQg.oYJXTg8_xN-eTNegOYkpsz94biY');
+client.login(process.env.TOKEN)
