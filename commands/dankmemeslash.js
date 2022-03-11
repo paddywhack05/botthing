90091ad779bec63de1e8e194845fd67da58305c5
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-03-11 19:51:32 +0000

    typo fix

--- commands/dankmemeslash.js
+++ commands/dankmemeslash.js
@@ -13,7 +13,7 @@ execute(interaction){
      allowModPost: true,
     }).then(post => {
         if(post.upvote_ratio >= 0.90){
-            interaction.channel.send(`here is a dank meme ${interaction.use.tag} ${post.title} ${post.url}`);
+            interaction.channel.send(`here is a dank meme ${interaction.user.tag} ${post.title} ${post.url}`);
          }else {
              interaction.channel.send(`im sorry but that post has a ${post.upvote_ratio} upvote ratio and may be offensive here is another`)
              interaction.channel.send("!dankmeme")
