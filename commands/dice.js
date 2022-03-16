3b97301415b97c88daee85675de8b325e271fe6a
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-03-16 16:25:12 +0000

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -2,12 +2,17 @@ module.exports= {
     name: "dice",
     description:"dice command",
     execute(message){
-        const rndInt = Math.floor(Math.random() * 6) + 1
+       
         message.reply ({content: "https://tenor.com/view/dice-roll-dice-roll-gif-23627860"})
         message.reply ({content: "rolling"})
-        setTimeout(1000)
+       
         if (rndInt == 1){
-        message.reply ({content: "https://tenor.com/view/dice-roll-dice-roll-gif-23627860"})
+            const rndInt = Math.floor(Math.random() * 6) + 1
+            function wait() {
+                message.reply ({content: "https://tenor.com/view/dice-roll-dice-roll-gif-23627860"})
+                console.log('wait call');
+              }
+              setTimeout(wait,1000);
         }
     }
 }
