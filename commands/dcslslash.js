af8723793b0733b14f220e7988438a9fba8488fb
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-03-25 17:36:29 +0000

    Update dcslslash.js

--- commands/dcslslash.js
+++ commands/dcslslash.js
@@ -3,7 +3,6 @@ module.exports= {
     description:"dcslslash command",
     execute(interaction,client){
 
-        interaction.reply ({content: "latest song https://youtu.be/jiXf3NzZOTA"})
-        interaction.reply({content: "Dcsl is a band at our school with many memebers"})
+        interaction.reply ({content: "latest song https://youtu.be/jiXf3NzZOTA\n Dcsl is a band at our school with many memebers"})
     }
 }
