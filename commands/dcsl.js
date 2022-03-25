c3edf554b6431c171c71c80a2a6f1a662f6013e9
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-03-25 18:35:34 +0000

    added dcsl website

--- commands/dcsl.js
+++ commands/dcsl.js
@@ -4,6 +4,7 @@ module.exports= {
     execute(message,client){
 
         message.reply ({content: "latest song https://youtu.be/jiXf3NzZOTA"})
-        message.reply({content: "Dcsl is a band at our school with many memebers"})
+        message.reply({content: "Dcsl is a band at our school with many memebers\n their website is http://www.dcsl.tk"})
+
     }
 }
