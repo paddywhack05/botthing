8d6db7130c3160f01d09e65ad340e877bb407b53
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-01 19:16:53 +0100

    s

--- commands/dcsl.js
+++ commands/dcsl.js
@@ -1,10 +1,37 @@
+const{MessageActionRow,MessageButton} = require('discord.js');
+const Discord = require('discord.js');
 module.exports= {
     name: "dcsl",
     description:"dcsl command",
     execute(message,client){
-
-        message.reply ({content: "latest song https://youtu.be/jiXf3NzZOTA"})
-        message.reply({content: "Dcsl is a band at our school with many memebers\n their website is https://dcsl.tk/"})
+        e();
+        async function e(){
+        const embed = new Discord.MessageEmbed()
+            .setColor('RED')
+            .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+            .setTitle(`Dcsl`)
+            .setURL(`https://dcsl.tk`)
+            .setDescription(`**[Latest song](https://youtu.be/jiXf3NzZOTA)**\n\n **SOCIALS**\n **[Youtube](https://www.youtube.com/channel/UC5Yic5Wio4klMgpdRzA9qeQ)** | **[Website](https://dcsl.tk)**`)
+            .setThumbnail(`https://yt3.ggpht.com/OzqCrvEc187vtkfwQtHq0WIU8l-eE5ZwVB3IVcTA2Nr8gGVj9hsZ1ekyyrrU1K6-xsPGJQFDiw=s88-c-k-c0x00ffffff-no-rj`)
+            .setFooter(`dcsl.tk`)
+            const row = new Discord.MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setEmoji(`🌐`)
+                .setLabel(`Website`)
+                .setURL(`https://dcsl.tk`)
+                .setStyle("LINK"),
+            )
+            .addComponents(
+                new MessageButton()
+                .setEmoji(`📽`)
+                .setLabel(`Youtube`)
+                .setURL(`https://www.youtube.com/channel/UC5Yic5Wio4klMgpdRzA9qeQ`)
+                .setStyle("LINK"),
+            )
+         
+          message.channel.send({ embeds: [embed],components: [row]});
+    }
 
     }
 }
--- commands/dcslslash.js
+++ commands/dcslslash.js
@@ -1,8 +1,36 @@
+const{MessageActionRow,MessageButton} = require('discord.js');
+const Discord = require('discord.js');
 module.exports= {
     name: "dcslslash",
     description:"dcslslash command",
     execute(interaction,client){
-
-        interaction.reply ({content: "latest song https://youtu.be/jiXf3NzZOTA \n Dcsl is a band at our school with many memebers\n their website is https://dcsl.tk/"})
+        e();
+        async function e(){
+        const embed = new Discord.MessageEmbed()
+            .setColor('RED')
+            .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+            .setTitle(`Dcsl`)
+            .setURL(`https://dcsl.tk`)
+            .setDescription(`**[Latest song](https://youtu.be/jiXf3NzZOTA)**\n\n **SOCIALS**\n **[Youtube](https://www.youtube.com/channel/UC5Yic5Wio4klMgpdRzA9qeQ)** | **[Website](https://dcsl.tk)**`)
+            .setThumbnail(`https://yt3.ggpht.com/OzqCrvEc187vtkfwQtHq0WIU8l-eE5ZwVB3IVcTA2Nr8gGVj9hsZ1ekyyrrU1K6-xsPGJQFDiw=s88-c-k-c0x00ffffff-no-rj`)
+            .setFooter(`dcsl.tk`)
+            const row = new Discord.MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setEmoji(`🌐`)
+                .setLabel(`Website`)
+                .setURL(`https://dcsl.tk`)
+                .setStyle("LINK"),
+            )
+            .addComponents(
+                new MessageButton()
+                .setEmoji(`📽`)
+                .setLabel(`Youtube`)
+                .setURL(`https://www.youtube.com/channel/UC5Yic5Wio4klMgpdRzA9qeQ`)
+                .setStyle("LINK"),
+            )
+         
+          await interaction.channel.send({ embeds: [embed],components: [row]});
     }
+}
 }
