1f8b2270101f648b19f2b744dcc9ff7f3cdbc156
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-04 18:08:12 +0100

    Update helpslash.js

--- commands/helpslash.js
+++ commands/helpslash.js
@@ -13,7 +13,7 @@ module.exports= {
             .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
             .setTitle(`Paddycrackbot Help`)
             .setURL(``)
-            .setDescription(`**COMMANDS**\n!meme, !infuriating, !cat, !dog, !minecraft, !dankmeme,\n !ukraine, !ukraine news, do a barrel roll, !rickroll !hack, !ascii \n\n**DOCUMENTATION**\n**Type /** to view the slash command options or **[look at the documentation](https://paddycrack.com/bot)** to see what it all does\n\n **SOCIALS**\n **[Youtube](https://www.youtube.com/channel/UCmd3UCWz0LXd0CHQ0g6vQ4A) | [Twitch](https://www.twitch.tv/paddycrack_) | [Discord](https://discord.gg/yFZXQB8cjx)**\n\n**LINKS**\n**[Website](https://paddycrack.com) | [Github](https://github.com/paddywhack05/botthing) | [Top.gg](https://top.gg/bot/916743866915389542)**`)
+            .setDescription(`**COMMANDS**\n!meme, !infuriating, !cat, !dog, !minecraft, !dankmeme,\n !ukraine, !ukraine news, do a barrel roll, !rickroll !hack, !ascii !vote, !invite, !info, !userinfo\n\n**DOCUMENTATION**\n**Type /** to view the slash command options or **[look at the documentation](https://paddycrack.com/bot)** to see what it all does\n\n **SOCIALS**\n **[Youtube](https://www.youtube.com/channel/UCmd3UCWz0LXd0CHQ0g6vQ4A) | [Twitch](https://www.twitch.tv/paddycrack_) | [Discord](https://discord.gg/yFZXQB8cjx)**\n\n**LINKS**\n**[Website](https://paddycrack.com) | [Github](https://github.com/paddywhack05/botthing) | [Top.gg](https://top.gg/bot/916743866915389542)**`)
             .setThumbnail(`${client.user.displayAvatarURL({ dynamic: true })}`)
             .setFooter(`paddycrack.com`)
             .setImage(`${client.user.displayAvatarURL({ dynamic: true })}`)
