277651bd88347adc7d48cf434ed28c754c8a618f
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-06 17:54:50 +0100

    Update userinfoslash.js

--- commands/userinfoslash.js
+++ commands/userinfoslash.js
@@ -1,5 +1,6 @@
 const{MessageActionRow,MessageButton} = require('discord.js');
 const Discord = require('discord.js');
+const moment = require('moment');
 module.exports= {
     name: "userinfoslash",
     description:"userinfo command",
@@ -12,7 +13,7 @@ module.exports= {
                 .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
                 .setTitle(`${interaction.user.username} information`)
                 .setURL(``)
-                .setDescription(`**${interaction.user.username} joined ${interaction.member.guild} at** ${interaction.member.joinedAt}\n **Nickname: ${interaction.member.nickname}** \n **User id: ${interaction.user.id}**\n **Role color: ${interaction.member.displayHexColor}**`)
+                .setDescription(`**${interaction.user.username} joined ${interaction.member.guild} at** ${moment(interaction.member.joinedAt).format('YYYY/MM/DD, h:mm:ss')}\n**Joined discord at** ${moment(interaction.user.createdAt).format('YYYY/MM/DD, h:mm:ss')} \n **Nickname: ${interaction.member.nickname}** \n **User id: ${interaction.user.id}**\n **Role color: ${interaction.member.displayHexColor}**`)
                 .setImage(`${interaction.user.displayAvatarURL({ dynamic: true })}`)
                 .setThumbnail(`${interaction.user.displayAvatarURL({ dynamic: true })}`)
                 .setFooter(`paddycrack.com`)
