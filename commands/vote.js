09bac04de6f746c923ae98a236df346f94365512
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-07 18:40:23 +0100

    Vote command update

--- commands/vote.js
+++ commands/vote.js
@@ -3,21 +3,32 @@ const Discord = require('discord.js');
 module.exports= {
     name: "vote",
     description:"vote command",
-    execute(message,client){
-       
-            e();
-            async function e(){
+    async execute(message,client){
+            
             const embed = new Discord.MessageEmbed()
                 .setColor('RED')
                 .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
-                .setTitle(`Vote for us on Top.gg`)
-                .setURL(`https://top.gg/bot/916743866915389542`)
-                .setDescription(`**[VOTE](https://top.gg/bot/916743866915389542)**`)
-                .setThumbnail(`https://yt3.ggpht.com/I17eliiYPS0ibWEjnkLt5okgtwi4pAkT2ne0EQZYKTjEUoRwuH4dUf4MpgBI4I3QnCQXQTP_5Iw=s88-c-k-c0x00ffffff-no-rj`)
-                .setImage('https://yt3.ggpht.com/I17eliiYPS0ibWEjnkLt5okgtwi4pAkT2ne0EQZYKTjEUoRwuH4dUf4MpgBI4I3QnCQXQTP_5Iw=s88-c-k-c0x00ffffff-no-rj')
+                .setTitle(`Vote for us`)
+                .setURL(`https://top.gg/bot/916743866915389542/vote`)
+                .setDescription(`**[VOTE ON TOP.GG](https://top.gg/bot/916743866915389542/vote)** | **[VOTE ON DBL](https://discordbotlist.com/bots/paddycrackbot/upvote)**`)
+                .setThumbnail(`${client.user.displayAvatarURL({ dynamic: true })}`)
                 .setFooter(`paddycrack.com`)
-              message.channel.send({ embeds: [embed]});
+                const row = new Discord.MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setEmoji(`🤖`)
+                    .setLabel(`Vote on Top.gg`)
+                    .setURL(`https://top.gg/bot/916743866915389542/vote`)
+                    .setStyle("LINK"),
+                )
+                .addComponents(
+                    new MessageButton()
+                    .setEmoji(`🤖`)
+                    .setLabel(`Vote on DBL`)
+                    .setURL(`https://discordbotlist.com/bots/paddycrackbot/upvote`)
+                    .setStyle("LINK"),
+                )
+              message.channel.send({ embeds: [embed],components:[row]});
             
-        }
     }
 }
--- commands/voteslash.js
+++ commands/voteslash.js
@@ -3,21 +3,32 @@ const Discord = require('discord.js');
 module.exports= {
     name: "voteslash",
     description:"voteslash command",
-    execute(interaction,client){
+    async execute(interaction,client){
        
-            e();
-            async function e(){
             const embed = new Discord.MessageEmbed()
                 .setColor('RED')
                 .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
-                .setTitle(`Vote for us on Top.gg`)
-                .setURL(`https://top.gg/bot/916743866915389542`)
-                .setDescription(`**[VOTE](https://top.gg/bot/916743866915389542)**`)
-                .setThumbnail(`https://yt3.ggpht.com/I17eliiYPS0ibWEjnkLt5okgtwi4pAkT2ne0EQZYKTjEUoRwuH4dUf4MpgBI4I3QnCQXQTP_5Iw=s88-c-k-c0x00ffffff-no-rj`)
-                .setImage('https://yt3.ggpht.com/I17eliiYPS0ibWEjnkLt5okgtwi4pAkT2ne0EQZYKTjEUoRwuH4dUf4MpgBI4I3QnCQXQTP_5Iw=s88-c-k-c0x00ffffff-no-rj')
+                .setTitle(`Vote for us`)
+                .setURL(`https://top.gg/bot/916743866915389542/vote`)
+                .setDescription(`**[VOTE ON TOP.GG](https://top.gg/bot/916743866915389542/vote)** | **[VOTE ON DBL](https://discordbotlist.com/bots/paddycrackbot/upvote)**`)
+                .setThumbnail(`${client.user.displayAvatarURL({ dynamic: true })}`)
                 .setFooter(`paddycrack.com`)
-              interaction.reply({ embeds: [embed]});
+                const row = new Discord.MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setEmoji(`🤖`)
+                    .setLabel(`Vote on Top.gg`)
+                    .setURL(`https://top.gg/bot/916743866915389542/vote`)
+                    .setStyle("LINK"),
+                )
+                .addComponents(
+                    new MessageButton()
+                    .setEmoji(`🤖`)
+                    .setLabel(`Vote on DBL`)
+                    .setURL(`https://discordbotlist.com/bots/paddycrackbot/upvote`)
+                    .setStyle("LINK"),
+                )
+              interaction.reply({ embeds: [embed],components:[row]});
             
-        }
     }
 }
