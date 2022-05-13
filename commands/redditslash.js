202d63ba1a356e1e1e1c35bc32699841a8bf1269
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-13 21:47:26 +0100

    Update redditslash.js

--- commands/redditslash.js
+++ commands/redditslash.js
@@ -78,56 +78,57 @@ async execute(interaction,client,text){
      
          
      });
-     async function nfetch(){
-        redditFetch({
-            
-            subreddit:`${text}`,
-            sort: `top`,
-            allowNSFW: true,
-        allowCrossPost: true,
-            allowVideo: false,
-            allowModPost: false,
-            
-         }).then(async post => {
-            // console.log(post);
-            
-                 if(post.url.includes("gallery")){fetch();return;}
-                 if(post.upvote_ratio >= 0.90){
-                    var anser = 1.00-post.upvote_ratio;
-               console.log(anser);
-               var dwn = anser * post.ups;
-               console.log(final);
-               var final = Math.round(dwn);
-                     
-                const embed = new Discord.MessageEmbed()
-                .setColor('RED')
-                .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
-                .setTitle(`${post.title}`)
-                .setURL(`https://redd.it/${post.id}`)
-                .setDescription(`${post.selftext}`)
-                .setThumbnail(`${post.url}`)
-                //.addField('field test','field description test')
-                .setImage(`${post.url}`)
-                .setFooter(`💬 ${post.num_comments} 👍 ${post.ups} 👎 ${final}`)
-                const row = new Discord.MessageActionRow()
-                .addComponents(
-                    new MessageButton()
-                    .setLabel(`r/${text}`)
-                    .setURL(`https://www.reddit.com/r/${text}/`)
-                    .setStyle("LINK"),
-                )
-               
-                await interaction.channel.send({ embeds: [embed],components: [row]});
-                console.log(`dwn ${post.downs} \n ups${post.upvote_ratio} \n real downs ${anser}`)
-                //message.channel.send(`here is a meme ${message.author} ${post.title} ${post.url}`);
-               
-             }else {
-                 nfetch();
-             }
+  
+}
+async function nfetch(){
+    redditFetch({
+        
+        subreddit:`${text}`,
+        sort: `top`,
+        allowNSFW: true,
+    allowCrossPost: true,
+        allowVideo: false,
+        allowModPost: false,
+        
+     }).then(async post => {
+        // console.log(post);
+        
+             if(post.url.includes("gallery")){fetch();return;}
+             if(post.upvote_ratio >= 0.90){
+                var anser = 1.00-post.upvote_ratio;
+           console.log(anser);
+           var dwn = anser * post.ups;
+           console.log(final);
+           var final = Math.round(dwn);
+                 
+            const embed = new Discord.MessageEmbed()
+            .setColor('RED')
+            .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+            .setTitle(`${post.title}`)
+            .setURL(`https://redd.it/${post.id}`)
+            .setDescription(`${post.selftext}`)
+            .setThumbnail(`${post.url}`)
+            //.addField('field test','field description test')
+            .setImage(`${post.url}`)
+            .setFooter(`💬 ${post.num_comments} 👍 ${post.ups} 👎 ${final}`)
+            const row = new Discord.MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setLabel(`r/${text}`)
+                .setURL(`https://www.reddit.com/r/${text}/`)
+                .setStyle("LINK"),
+            )
+           
+            await interaction.channel.send({ embeds: [embed],components: [row]});
+            console.log(`dwn ${post.downs} \n ups${post.upvote_ratio} \n real downs ${anser}`)
+            //message.channel.send(`here is a meme ${message.author} ${post.title} ${post.url}`);
+           
+         }else {
+             nfetch();
+         }
+     
          
-             
-         });
-      
-    }
+     });
+  
 }
 }}
