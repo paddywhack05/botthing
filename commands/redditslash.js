f7b3442aae809808db6aa1793273ec77e68ee72e
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-13 21:44:06 +0100

    Update redditslash.js

--- commands/redditslash.js
+++ commands/redditslash.js
@@ -15,6 +15,10 @@ async execute(interaction,client,text){
         if (data.error === 404||data.error === 403||data.data.dist === 0){
             interaction.channel.send(`subreddit not found reason ${data.reason} message ${data.message} err code ${data.error} \n if it says undefind under message or err there was not enough data or a sub called that was deleted`);
         }
+        else if (interaction.channel.nsfw){
+            console.log("nsfw chanel detected")
+            nfetch();
+        }
         else{rfetch();}
     
     }
@@ -74,6 +78,56 @@ async execute(interaction,client,text){
      
          
      });
-  
+     async function nfetch(){
+        redditFetch({
+            
+            subreddit:`${text}`,
+            sort: `top`,
+            allowNSFW: true,
+        allowCrossPost: true,
+            allowVideo: false,
+            allowModPost: false,
+            
+         }).then(async post => {
+            // console.log(post);
+            
+                 if(post.url.includes("gallery")){fetch();return;}
+                 if(post.upvote_ratio >= 0.90){
+                    var anser = 1.00-post.upvote_ratio;
+               console.log(anser);
+               var dwn = anser * post.ups;
+               console.log(final);
+               var final = Math.round(dwn);
+                     
+                const embed = new Discord.MessageEmbed()
+                .setColor('RED')
+                .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+                .setTitle(`${post.title}`)
+                .setURL(`https://redd.it/${post.id}`)
+                .setDescription(`${post.selftext}`)
+                .setThumbnail(`${post.url}`)
+                //.addField('field test','field description test')
+                .setImage(`${post.url}`)
+                .setFooter(`💬 ${post.num_comments} 👍 ${post.ups} 👎 ${final}`)
+                const row = new Discord.MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setLabel(`r/${text}`)
+                    .setURL(`https://www.reddit.com/r/${text}/`)
+                    .setStyle("LINK"),
+                )
+               
+                await interaction.channel.send({ embeds: [embed],components: [row]});
+                console.log(`dwn ${post.downs} \n ups${post.upvote_ratio} \n real downs ${anser}`)
+                //message.channel.send(`here is a meme ${message.author} ${post.title} ${post.url}`);
+               
+             }else {
+                 nfetch();
+             }
+         
+             
+         });
+      
+    }
 }
 }}
