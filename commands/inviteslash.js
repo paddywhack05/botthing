cb8edec047c730e63d5d1b219ddbf407078869d9
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-14 17:23:03 +0100

    Update inviteslash.js

--- commands/inviteslash.js
+++ commands/inviteslash.js
@@ -12,25 +12,32 @@ module.exports= {
             .setAuthor(`${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
             .setTitle(`Invite Paddycrackbot to your server`)
             .setURL(`https://discord.com/api/oauth2/authorize?client_id=916743866915389542&permissions=8&scope=applications.commands%20bot`)
-            .setDescription(`**[Invite me](https://discord.com/api/oauth2/authorize?client_id=916743866915389542&permissions=8&scope=applications.commands%20bot) \n [Top.gg](https://top.gg/bot/916743866915389542)**`)
             .setThumbnail(`${client.user.displayAvatarURL({ dynamic: true })}`)
-            .setFooter(`paddycrack.com`)
             .setImage(`${client.user.displayAvatarURL({ dynamic: true })}`)
-            const row = new Discord.MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                .setEmoji(`🌐`)
-                .setLabel(`Top.gg`)
-                .setURL(`https://top.gg/bot/916743866915389542`)
-                .setStyle("LINK"),
-            )
-            .addComponents(
-                new MessageButton()
-                .setEmoji(`📨`)
-                .setLabel(`Invite`)
-                .setURL(`https://discord.com/api/oauth2/authorize?client_id=916743866915389542&permissions=8&scope=applications.commands%20bot`)
-                .setStyle("LINK"),
-            )
+            .setDescription(`**[Invite me](https://discord.com/api/oauth2/authorize?client_id=916743866915389542&permissions=8&scope=applications.commands%20bot) \n[Top.gg](https://top.gg/bot/916743866915389542)\n[DBL](https://discordbotlist.com/bots/paddycrackbot)**`)
+                .setFooter(`paddycrack.com`)
+                const row = new Discord.MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setEmoji(`📨`)
+                    .setLabel(`Invite`)
+                    .setURL(`https://discord.com/api/oauth2/authorize?client_id=916743866915389542&permissions=8&scope=applications.commands%20bot`)
+                    .setStyle("LINK"),
+                )
+                .addComponents(
+                    new MessageButton()
+                    .setEmoji(`🌐`)
+                    .setLabel(`Top.gg`)
+                    .setURL(`https://top.gg/bot/916743866915389542`)
+                    .setStyle("LINK"),
+                )
+                .addComponents(
+                    new MessageButton()
+                    .setEmoji(`🤖`)
+                    .setLabel(`DBL`)
+                    .setURL(`https://discordbotlist.com/bots/paddycrackbot`)
+                    .setStyle("LINK"),
+                )
           await interaction.reply({ embeds: [embed],components: [row]});
     }
    }
