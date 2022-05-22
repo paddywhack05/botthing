689676cc28c82b040283e0e485eca32e8f47965e
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-05-22 19:26:57 +0100

    fuck my brother he a bitch

--- commands/userinfoslash.js
+++ commands/userinfoslash.js
@@ -16,26 +16,43 @@ module.exports= {
                const Member = interaction.guild.members.cache.get(Target.id)
                if(!Member){
                 interaction.reply('This user is not in the server');return;}
-           const embed = new Discord.MessageEmbed()
-               .setColor(Member.displayHexColor||'RED')
-               .setAuthor(`${Target.tag}`, Target.displayAvatarURL({ dynamic: true }))
-               .setTitle(`${Target.username} information`)
-               .setImage(`${Target.displayAvatarURL({ dynamic: true })}`)
-               .setThumbnail(`${Target.displayAvatarURL({ dynamic: true })}`)
-               .setURL(``)
-               .setDescription(`**${Target.username} joined ${Member.guild} at** ${moment(Member.joinedAt).format('YYYY/MM/DD, h:mm:ss')}\n**Joined discord at** **Joined discord at** ${moment(Target.createdAt).format('YYYY/MM/DD, h:mm:ss')}\n**Nickname: ${Member.nickname}**\n**User id: ${Target.id}**\n**Role color: ${Member.displayHexColor}**\n`,"Roles",`${Member.roles.cache.map(r=>r).join(' ').replace("@everyone"," ")}`)
-               /*.addField(`**${Target.username} joined ${Member.guild} at** ${moment(Member.joinedAt).format('YYYY/MM/DD, h:mm:ss')}`)
-               /*.addField(`**Joined discord at** ${moment(Target.createdAt).format('YYYY/MM/DD, h:mm:ss')}`)
-               .addField(`**Nickname: ${Member.nickname}**`)
-               .addField(`**User id: ${Target.id}**`,false)
-               .addField(`**Role color: ${Member.displayHexColor}`)*/
-               .addField("Roles",`${Member.roles.cache.map(r=>r).join(' ')}`)
-               .setFooter(`paddycrack.com`)
-              
-                  
-                
-                 interaction.reply({ embeds: [embed]});
-               
+                const rol = Member.roles.cache.map(r=>r).length - 1;
+                if(rol === 0){
+                    const embed = new Discord.MessageEmbed()
+                    .setColor(Member.displayHexColor||'RED')
+                    .setAuthor(`${Target.tag}`, Target.displayAvatarURL({ dynamic: true }))
+                    .setTitle(`${Target.username} information`)
+                    .setImage(`${Target.displayAvatarURL({ dynamic: true })}`)
+                    .setThumbnail(`${Target.displayAvatarURL({ dynamic: true })}`)
+                    .setURL(``)
+                    .setDescription(`**${Target.username} joined ${Member.guild} at** ${moment(Member.joinedAt).format('YYYY/MM/DD, h:mm:ss')}\n**Joined discord at** **Joined discord at** ${moment(Target.createdAt).format('YYYY/MM/DD, h:mm:ss')}\n**Nickname: ${Member.nickname}**\n**User id: ${Target.id}**\n**Role color: ${Member.displayHexColor}**\n`)
+                    /*.addField(`**${Target.username} joined ${Member.guild} at** ${moment(Member.joinedAt).format('YYYY/MM/DD, h:mm:ss')}`)
+                    /*.addField(`**Joined discord at** ${moment(Target.createdAt).format('YYYY/MM/DD, h:mm:ss')}`)
+                    .addField(`**Nickname: ${Member.nickname}**`)
+                    .addField(`**User id: ${Target.id}**`,false)
+                    .addField(`**Role color: ${Member.displayHexColor}`)*/
+                    .addField(`Roles[0]`,`No roles lol dont think \@everyone counts`)
+                    .setFooter(`paddycrack.com`)
+                    interaction.channel.send({ embeds: [embed]});
+                }
+                else{
+            const embed = new Discord.MessageEmbed()
+                .setColor(Member.displayHexColor||'RED')
+                .setAuthor(`${Target.tag}`, Target.displayAvatarURL({ dynamic: true }))
+                .setTitle(`${Target.username} information`)
+                .setImage(`${Target.displayAvatarURL({ dynamic: true })}`)
+                .setThumbnail(`${Target.displayAvatarURL({ dynamic: true })}`)
+                .setURL(``)
+                .setDescription(`**${Target.username} joined ${Member.guild} at** ${moment(Member.joinedAt).format('YYYY/MM/DD, h:mm:ss')}\n**Joined discord at** **Joined discord at** ${moment(Target.createdAt).format('YYYY/MM/DD, h:mm:ss')}\n**Nickname: ${Member.nickname}**\n**User id: ${Target.id}**\n**Role color: ${Member.displayHexColor}**\n`)
+                /*.addField(`**${Target.username} joined ${Member.guild} at** ${moment(Member.joinedAt).format('YYYY/MM/DD, h:mm:ss')}`)
+                /*.addField(`**Joined discord at** ${moment(Target.createdAt).format('YYYY/MM/DD, h:mm:ss')}`)
+                .addField(`**Nickname: ${Member.nickname}**`)
+                .addField(`**User id: ${Target.id}**`,false)
+                .addField(`**Role color: ${Member.displayHexColor}`)*/
+                .addField(`Roles[${rol}]`,`${Member.roles.cache.map(r=>r).join(' ').replace("@everyone",'.')}`)
+                .setFooter(`paddycrack.com`)
+                interaction.channel.send({ embeds: [embed]});
+                }
                }
            }
     
